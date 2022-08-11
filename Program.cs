3d9692f6f770edf91bab028470f5a89778babe19
hunterpuu <104664980+0psdev@users.noreply.github.com>
2022-08-11 21:05:44 +0700

    Update all code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BookStoreApi.Services;
+using BookStoreApi.Models;
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
